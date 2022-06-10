c005662bfbc97deefcfe72b6c8184e429f08806a
chris-tollins <105399625+chris-tollins@users.noreply.github.com>
2022-06-10 16:28:55 -0400

    Update main.js
    Adding of proper JS code to the file for review with Mr. Leftly.

--- main.js
+++ main.js
@@ -1,2 +1,39 @@
+window.onload = function() {
+  Formio.createForm(
+    document.getElementById("formio"),
+    "https://njcrwmbyfaaqryy.form.io/inovainc"
+  ).then((form) => {
+    form.on("change", (changed) => {
+      console.log(changed.data)
+      	const nav = document.querySelector(".formio-wizard-nav-container");
+      if (changed.data.newClient === "Get a Quote") {
+        nav.classList.add("hidden")
+      } else {
+      	nav.classList.remove("hidden")
+      }
+    });
 
-Hello World!
+    form.on("nextPage", ({
+      page,
+      submission
+    }) => {
+
+      const submitButton = document.querySelector(
+        ".btn.btn-primary.btn-wizard-nav-submit"
+      );
+      if (submitButton && page>1) {
+        if (page + 1 === form.pages.length) {
+          submitButton.classList.add("visible")
+        }
+      }
+
+    });
+    form.on("prevPage", ({
+      page,
+      submission
+    }) => {});
+    form.on("submit", (submission) => {
+      $("#myModal").modal("show");
+    });
+  });
+};
